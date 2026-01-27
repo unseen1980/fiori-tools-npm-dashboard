7cf2de26004220ac7f18cb192e16fe213b4f5ee5
Christos Koutsiaris <christos.koutsiaris@sap.com>
2026-01-27 11:42:49 +0000

    Remove duplicate search box from header

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -45,47 +45,7 @@ export default function Layout({ children }: LayoutProps) {
             </div>
 
             <div className="flex items-center justify-end ml-auto">
-              <div className="xs:hidden sm:hidden">
-                <button
-                  type="button"
-                  className="inline-flex flex-shrink-0 justify-center items-center gap-2 h-[2.375rem] w-[2.375rem] rounded-full font-medium bg-white text-gray-700 align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-white transition-all text-xs dark:bg-gray-800 dark:hover:bg-slate-800 dark:text-gray-400 dark:hover:text-white dark:focus:ring-gray-700 dark:focus:ring-offset-gray-800"
-                >
-                  <svg
-                    className="w-3.5 h-3.5"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-                  </svg>
-                </button>
-              </div>
-
-              {dataContext && (
-                <div className="hidden sm:block">
-                  <label htmlFor="icon" className="sr-only">
-                    Search
-                  </label>
-                  <div className="relative">
-                    <div className="absolute inset-y-0 left-0 flex items-center pointer-events-none z-20 pl-4">
-                      <svg
-                        className="h-4 w-4 text-gray-400"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        fill="currentColor"
-                        viewBox="0 0 16 16"
-                      >
-                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-                      </svg>
-                    </div>
-
-                    <AutoComplete data={autoCompleteData} />
-                  </div>
-                </div>
-              )}
+              {/* Search box removed - using the one on the main page instead */}
             </div>
           </nav>
         </header>
